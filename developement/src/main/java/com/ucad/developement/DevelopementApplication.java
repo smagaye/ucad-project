6cb8d294832da195474548e5488fa82ddf5fcc30
Sma Gaye <sma.gaye94@gmail.com>
2019-06-11 11:18:59 +0000

    Setup project

---
+++ developement/src/main/java/com/ucad/developement/DevelopementApplication.java
@@ -0,0 +1,13 @@
+package com.ucad.developement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopementApplication.class, args);
+    }
+
+}
